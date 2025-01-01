ebff52284dea114bdf69115e920b612bfe91645a
Nourredine <nzaher1981@gmail.com>
2025-01-01 13:17:32 +0100

    Initialize sauvegarde-travail

---
+++ MyJavaClass (2).java
@@ -0,0 +1,23 @@
+<<<<<<< HEAD
+
+
+
+
+public class MyJavaClass {
+    public static void main(String[] args) {
+        System.out.println("Bonjour depuis le code Java POO !");
+    }
+}
+
+=======
+
+
+
+
+public class MyJavaClass {
+    public static void main(String[] args) {
+        System.out.println("Bonjour depuis le code Java POO !");
+    }
+}
+
+>>>>>>> b58451c (mise a jour)
